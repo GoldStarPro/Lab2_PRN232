16126d2e732f189e6ece51798fe708848737a501
Tran Huy Hoang <huyhoangero@gmail.com>
2025-06-27 15:37:21 +0700

    Scaffold Models in BusinessObjects|Config ConnectionStrings

---
+++ BusinessObjects/CosmeticCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObjects;
+
+public partial class CosmeticCategory
+{
+    public string CategoryId { get; set; } = null!;
+
+    public string CategoryName { get; set; } = null!;
+
+    public string UsagePurpose { get; set; } = null!;
+
+    public string FormulationType { get; set; } = null!;
+
+    public virtual ICollection<CosmeticInformation> CosmeticInformations { get; set; } = new List<CosmeticInformation>();
+}

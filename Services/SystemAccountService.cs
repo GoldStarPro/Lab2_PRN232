14b457e21db00b0eb14c6ec02bb1071b110d2558
Tran Huy Hoang <huyhoangero@gmail.com>
2025-06-27 23:04:40 +0700

    Services Class Library

---
+++ Services/SystemAccountService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects;
+using Repositories;
+
+namespace Services
+{
+    public class SystemAccountService : ISystemAccountService
+    {
+        private readonly ISystemAccountRepository _repo;
+        public SystemAccountService(ISystemAccountRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<SystemAccount> Login(string email, string password)
+        {
+            return await _repo.Login(email, password);
+        }
+    }
+}

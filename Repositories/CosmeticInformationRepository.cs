2e200de189a8858d861032199bdb484901b35bb8
Tran Huy Hoang <huyhoangero@gmail.com>
2025-06-27 22:54:28 +0700

    Repositories Class Library

---
+++ Repositories/CosmeticInformationRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects;
+using DataAccessObjects;
+
+namespace Repositories
+{
+    public class CosmeticInformationRepository : ICosmeticInformationRepository
+    {
+        public async Task<CosmeticInformation> Add(CosmeticInformation cosmeticInformation) => await CosmeticInformationDAO.Instance.AddCosmeticInformation(cosmeticInformation);
+
+        public async Task<CosmeticInformation> Delete(string id) => await CosmeticInformationDAO.Instance.Delete(id);
+
+        public async Task<List<CosmeticCategory>> GetAllCategories() => await CosmeticInformationDAO.Instance.GetAllCategories();
+
+        public async Task<List<CosmeticInformation>> GetAllCosmetics() => await CosmeticInformationDAO.Instance.GetAllCosmetic();
+
+        public async Task<CosmeticInformation> GetOne(string id) => await CosmeticInformationDAO.Instance.GetById(id);
+
+        public Task<CosmeticInformation> Update(CosmeticInformation cosmeticInformation) => CosmeticInformationDAO.Instance.Update(cosmeticInformation);
+    }
+}
---
+++ Repositories/SystemAccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects;
+using DataAccessObjects;
+
+namespace Repositories
+{
+    internal class SystemAccountRepository : ISystemAccountRepository
+    {
+        public async Task<SystemAccount> Login(string email, string password)
+        {
+            return await SystemAccountDAO.Instance.Login(email, password);
+        }
+    }
+}
